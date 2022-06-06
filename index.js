ff2493e123d2b36e31880b40bf3f4c10e27b3201
Nicholas <nicholas.nmalinda@gmail.com>
2022-06-06 07:39:54 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "YOUR- NAME is the champion"
+
+document.body.h1.append.newHeader;
